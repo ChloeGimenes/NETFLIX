41692f0d534da740884ad9a5684be8746ba604dd
Chloe Gimenes <chloegimenes@gmail.com>
2019-12-27 13:46:04 +0100

    HeaderImg middle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Header } from "./components";
-import { Home } from "./routes";
+import { Home } from './routes';
 
 class App extends Component {
   render() {
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,49 +4,46 @@ import { HeaderImg } from '../components';
 
 const movies = [
   {
-    backdrop_path: require("./n6bUvigpRFqSwmPp1m2YADdbRBc.jpg"),
+    backdrop_path: '../../images/Fast_large.jpg',
     id: 475557,
     overview:
       "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
-    poster_path: require('/tWjJ3ILjsbTwKgXxEv48QAbYZ19.jpg'),
+    poster_path: '../../images/Fast_small.jpg',
     title: "Joker"
   },
   {
-    backdrop_path: require("/n6bUvigpRFqSwmPp1m2YADdbRBc.jpg"),
+    backdrop_path: '../images/Fast_large.jpg',
     id: 475558,
     overview:
       "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
-    poster_path: require('/tWjJ3ILjsbTwKgXxEv48QAbYZ19.jpg'),
+    poster_path: '../images/Fast_small.jpg',
     title: "Joker"
   },
   {
-    backdrop_path: "/n6bUvigpRFqSwmPp1m2YADdbRBc.jpg",
+    backdrop_path: '../images/Fast_large.jpg',
     id: 475559,
     overview:
       "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
-    poster_path: require('/tWjJ3ILjsbTwKgXxEv48QAbYZ19.jpg'),
+    poster_path: '../images/Fast_small.jpg',
     title: "Joker"
   },
   {
-    backdrop_path: require('/n6bUvigpRFqSwmPp1m2YADdbRBc.jpg'),
+    backdrop_path: '../images/Fast_large.jpg',
     id: 475554,
     overview:
       "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
-    poster_path: require('../tWjJ3ILjsbTwKgXxEv48QAbYZ19.jpg'),
+    poster_path: '../images/Fast_.jpg',
     title: "Joker"
   }
 ];
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+ 
   render() {
     return <div><HeaderImg 
       title="Fast an Furious"
        overview="Lorem Ipsum"
-       imgSrc={require('../../public/images/Fast_large.jpg')}
+       imgSrc='./images/Fast_large.jpg'
 
     /></div>;
   }
